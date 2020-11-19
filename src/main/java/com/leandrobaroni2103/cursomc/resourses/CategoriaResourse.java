4faac5d92bed3767cbb97be1d437f466887e27c8
Leandro Baroni <leandrobaroni2103@hotmail.com>
2020-11-19 02:37:45 -0300

    Testando a classe dominio Categoria

--- src/main/java/com/leandrobaroni2103/cursomc/resourses/CategoriaResourse.java
+++ src/main/java/com/leandrobaroni2103/cursomc/resourses/CategoriaResourse.java
@@ -1,15 +1,28 @@
 package com.leandrobaroni2103.cursomc.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.leandrobaroni2103.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResourse {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		System.out.println(123);
+		Categoria cat1 = new Categoria(1, "Informatica");
+		System.out.println(324);
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		List<Categoria> lista = new ArrayList<>();
+		System.out.println(546);
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
