a0d354d7dac1e927004463ee634121fea4f61578
Leandro Baroni <leandrobaroni2103@hotmail.com>
2020-11-23 01:05:51 -0300

    Produto e associação ManyToMany

--- src/main/java/com/leandrobaroni2103/cursomc/domain/Categoria.java
+++ src/main/java/com/leandrobaroni2103/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.leandrobaroni2103.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -17,6 +20,9 @@ public class Categoria implements Serializable{
 	private Integer id;
 	
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 
 	@Override
 	public int hashCode() {
@@ -65,5 +71,13 @@ public class Categoria implements Serializable{
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 }
