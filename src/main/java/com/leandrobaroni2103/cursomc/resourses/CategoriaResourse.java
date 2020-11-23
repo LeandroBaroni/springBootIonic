4e9d24616a08ad28e23133735172095757f314f5
Leandro Baroni <leandrobaroni2103@hotmail.com>
2020-11-22 23:10:40 -0300

    Criando Repository e Service

--- src/main/java/com/leandrobaroni2103/cursomc/resourses/CategoriaResourse.java
+++ src/main/java/com/leandrobaroni2103/cursomc/resourses/CategoriaResourse.java
@@ -3,26 +3,26 @@ package com.leandrobaroni2103.cursomc.resourses;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leandrobaroni2103.cursomc.domain.Categoria;
+import com.leandrobaroni2103.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResourse {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		System.out.println(123);
-		Categoria cat1 = new Categoria(1, "Informatica");
-		System.out.println(324);
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		List<Categoria> lista = new ArrayList<>();
-		System.out.println(546);
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+	@Autowired
+	private CategoriaService service;
+
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
