1b8a70db4cf6e03619dcf009c6ca141234830886
Leandro Baroni <leandrobaroni2103@hotmail.com>
2020-12-03 03:26:39 -0300

    Validação de email em update de Cliente

--- src/main/java/com/leandrobaroni2103/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/leandrobaroni2103/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.leandrobaroni2103.cursomc.domain.Cliente;
+import com.leandrobaroni2103.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
